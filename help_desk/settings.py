69ad9fcac8e1b9173c5f549296a5bde91fd841af
Your \Name <laminjawla1001@gmail.com>
2023-02-17 15:10:54 +0000

    Environment variables

--- help_desk/settings.py
+++ help_desk/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-m%4!kwno$%!35lar6pnwn)4=w%@ma!--ra@===*pr%@tt!utn$"
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = str(os.environ.get('DEBUG')) == "1"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
