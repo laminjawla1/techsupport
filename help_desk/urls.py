62f4e50ef8a10f94fdc6e54040e9a90b718a82af
Lamin Jawla <laminjawla1001@gmail.com>
2023-03-04 19:38:06 +0000

    Static files

--- help_desk/urls.py
+++ help_desk/urls.py
@@ -23,7 +23,7 @@ from ticket.views import BookTicketView, TicketView, UpdateTicketView, Dashboard
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("blog.urls")),
+    path("", include("blog.urls"), name='blog'),
     path("login/", auth_views.LoginView.as_view(template_name="accounts/login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(template_name="accounts/logout.html"), name="logout"),
     path("password_reset/", 
