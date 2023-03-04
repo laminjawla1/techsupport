f086804fd5ea15d9e3e8b4d120fd0e091d293f6f
Lamin Jawla <laminjawla1001@gmail.com>
2023-03-04 18:09:17 +0000

    Fix static files route

--- help_desk/urls.py
+++ help_desk/urls.py
@@ -44,6 +44,7 @@ urlpatterns = [
 ] 
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 admin.site.site_header = "Yonna Forex Bureau - IT Help-Desk Admin"
 admin.site.site_title = "Yonna Tech Support Admin Portal"
