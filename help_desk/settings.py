a26fc05beffc476980750f190b1522f5ddbcb1f0
Lamin Jawla <laminjawla1001@gmail.com>
2023-03-04 18:18:55 +0000

    Fix static files route

--- help_desk/settings.py
+++ help_desk/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "staticfiles/"
+STATIC_URL = "/staticfiles/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
